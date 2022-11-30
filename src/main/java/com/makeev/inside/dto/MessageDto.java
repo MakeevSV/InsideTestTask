166e9c5ace40ab76d094372437049d436b00b964
Makeev Sergej <makeev.sev@gmail.com>
2022-12-01 00:49:25 +0900

    Second day 4/1

--- src/main/java/com/makeev/inside/dto/MessageDto.java
+++ src/main/java/com/makeev/inside/dto/MessageDto.java
@@ -1,5 +1,6 @@
 package com.makeev.inside.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,7 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class MessageDto implements Serializable {
 
     private  String name;
---
+++ src/test/java/com/makeev/inside/service/MessageServiceTest.java
@@ -0,0 +1,21 @@
+package com.makeev.inside.service;
+
+import org.junit.jupiter.api.Test;
+
+
+class MessageServiceTest {
+
+    @Test
+    void newMessage() {
+    }
+
+    @Test
+    void getLastNMessage() {
+    }
+
+    @Test
+    void keyChecker() {
+
+
+    }
+}
